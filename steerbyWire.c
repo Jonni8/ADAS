47eb2c75b7717122e95bd78903f8fecafd69566e
jonny <jonathan.alban897@gmail.com>
2023-04-25 16:25:54 +0200

    implementazione del steerbywire

--- steerbyWire.c
+++ steerbyWire.c
@@ -3,7 +3,10 @@
 #include <stdlib.h>
 #include <time.h>
 
-void printFor4sec(FILE *f, char *string) {
+char sinistra[] = "STO GIRANDO A SINISTRA\n";
+char destra[] = "STO GIRANDO A DESTRA\n";
+
+void printFor4sec(FILE *f, const char string[]) {
     time_t start, end;
     double elapsed, prev_elapsed = 0.0;
     time(&start);  
@@ -11,44 +14,34 @@ void printFor4sec(FILE *f, char *string) {
         time(&end);
         elapsed = difftime(end, start);
         if (elapsed >= prev_elapsed+1.0)
-        {
-            putc(*string, f);
-            //printf("DESTRA \n");
-            *string++;
+        {   
+            fputs(string, f);
             prev_elapsed = elapsed;
         }
     } while(elapsed < 4.0);
 }
 
-int main(int argc, char *argv[]) {
-    int i = 1;
-    char *command = argv[1];
+void steer_by_wheel (char command[]) {
+    const char dx[] = "DESTRA";
+    const char sx[] = "SINISTRA"; 
     FILE *fdw = fopen("steer.log", "w");
-    printf("%s \n",command);
-    //const char *dx = "DESTRA"; 
-    //int value = (char) strcmp(&command, dx);
-    //printf("%d", &dx);
-    
-    printFor4sec(fdw, command);
-    
-    //steer_by_wheel(command);
+    int value = strcmp(command, dx);
+    if ( value == 0 ) {
+        printFor4sec(fdw, destra);
+        fclose(fdw);
+    } else {
+        printFor4sec(fdw, sinistra);
+        fclose(fdw);
+    }
+}
 
+int main(int argc, char *argv[]) {
+    char *command = argv[1];
+    steer_by_wheel(command);
    }
 
-/*
-void steer_by_wheel (char *command) {
-    const char *dx = "DESTRA";
-    const char *sx = "SINISTRA"; 
-    int value = strcmp(command, dx);
-    FILE *fdw = fopen("steer.log", "w");
-    if ( value == 0 ) {
-        printFor4sec(*dx, &fdw);
-    } else {
-        printFor4sec(*sx, &fdw);
-    }
 
-}
-*/
+
 
 
 
